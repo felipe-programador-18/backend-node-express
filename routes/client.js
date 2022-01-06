344e1bf7bd6214cddc28576b71511cfd84cb1dad
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-06 17:24:16 -0300

    c9- get started create middalware in the page back end

--- routes/client.js
+++ routes/client.js
@@ -2,11 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 // here i take and before name change to router!!
-router.get('/client/list', (req, res) =>{
+router.get('/list', (req, res) =>{
     res.send(' here list now')
  })
- 
-router.get('/client/new', (req, res) =>{
+
+router.get('/new', (req, res) =>{
     res.send('Page of clients')
  })
 
