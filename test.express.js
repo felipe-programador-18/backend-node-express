ba0e533ef9ee4eb777d5c0cc56ae9515f67c9999
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-06 15:56:42 -0300

    c3- get started in create router to practice back-end

--- test.express.js
+++ test.express.js
@@ -7,5 +7,14 @@ const poor = 3000
 api.get('/', (req, res) =>{
    res.send('Felipe Programer 18')
 })
+api.get('/page', (req, res) =>{
+   res.send('Felipe smarer ')
+})
+api.get('/client/list', (req, res) =>{
+   res.send(' here list now')
+})
+api.get('/client/new', (req, res) =>{
+   res.send('Page of clients')
+})
 
 api.listen(poor, () => console.log('listing of poor' + poor))
