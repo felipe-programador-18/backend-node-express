b572bf396cc84d0f179cddbd3003e0d71f38e11d
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-06 16:22:33 -0300

    c7-addind new file index and trade somethings

--- test.express.js
+++ test.express.js
@@ -6,14 +6,10 @@ const poor = 3000
 
 //create way for acess router
 const indexRouter = require('./routes/index')
+const clientRouter = require('./routes/client')
 
-api.get('/client/list', (req, res) =>{
-   res.send(' here list now')
-})
-api.get('/client/new', (req, res) =>{
-   res.send('Page of clients')
-})
 
 api.listen(poor, () => console.log('listing of poor' + poor))
 
 api.use('/', indexRouter)
+api.use('/', clientRouter)
