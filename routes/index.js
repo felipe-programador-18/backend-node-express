d2e1ee53536da5e6630bee70691aa8abbaa30f84
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-06 16:04:36 -0300

    c5- export module routes and uses apiuse

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express')
-const Router = express.Router()
+const router = express.Router()
 
 //here i take o name before was api and trade for Router of rotas!!!
-Router.get('/', (req, res) =>{
+router.get('/', (req, res) =>{
     res.send('Felipe Programer 18')
  })
 
- Router.get('/page', (req, res) =>{
+router.get('/page', (req, res) =>{
     res.send('Felipe smarer ')
  })
 
 
- module.exports = Router
+ module.exports = router
