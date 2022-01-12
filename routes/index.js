889f5bfc8c330181aafd346bffc5338a35d29346
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-12 18:00:13 -0300

    c13-adding new modify to change my code

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+// Have export and controllers here now!!
+const Time = require('../controllers/index')
+const Moretime = require('../controllers/index')
 // here i take and before name change to router!!
-router.get('/', (req, res) =>{
-    res.send('Felipe Programer 18')
- })
-
-router.get('/page', (req, res) =>{
-    res.send('Felipe smarer ')
- })
+router.get('/', Time)
+router.get('/page',Moretime)
 
 
  module.exports = router
