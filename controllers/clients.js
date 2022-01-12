da3d8095b3fbdfe526837db0c887976ffd0f5318
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-12 18:02:07 -0300

    c15-adding new file about controllers to change and improve my code in back end

---
+++ controllers/clients.js
@@ -0,0 +1,14 @@
+
+const list = (req, res) =>{
+    res.send(' here list now')
+ }
+
+const Newlist = (req, res) =>{
+    res.send('Page of clients')
+ }
+
+
+module.exports= {
+    list,
+    Newlist
+}
---
+++ controllers/index.js
@@ -0,0 +1,15 @@
+
+const recept = (req, res) =>{
+    res.send('Felipe Programer 18')
+ }
+
+const caught = (req, res) =>{
+    res.send('Felipe smarer ')
+ }
+
+
+module.exports = {
+    recept,
+    caught
+
+} 
