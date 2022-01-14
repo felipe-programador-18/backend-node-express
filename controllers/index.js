ce36450ceae00ad89e9e77f6c1baa455c88c2c02
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-14 17:03:23 -0300

    c20- create query string and append reference

--- controllers/index.js
+++ controllers/index.js
@@ -7,9 +7,23 @@ const caught = (req, res) =>{
     res.send('Felipe smarer ')
  }
 
+const calc = (req, res) => {
+    console.log(req.query)
+
+    // remember i have always use destrucment assisment to improve my code always!!!
+     if(req.query.num1 && req.query.num2){
+      // now uses about destrucment assiment
+        const {num1, num2} = req.query
+        const soma = parseFloat(num1) + parseFloat(num2)
+        res.send('A soma é :' + soma)
+      } else{
+    res.send('calculator')
+      }
+}
 
 module.exports = {
     recept,
-    caught
+    caught,
+    calc
 
 } 
