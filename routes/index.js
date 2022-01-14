adef43c25f37ad4624df0dfb3728d00b63ad7643
FeProg-18 <79095518+felipe-programador-18@users.noreply.github.com>
2022-01-14 17:04:00 -0300

    c21-adding new routes to make new controllers

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const Moretime = require('../controllers/index')
 // here i take and before name change to router!!
 router.get('/', Time.recept)
 router.get('/page',Moretime.caught)
-
+// now i am apply about query string!!
+router.get('/calc', Time.calc)
 
  module.exports = router
